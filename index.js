7af39a4a1783678e81e7f0521113fecf2cc1e006
Anurag <danurag1906@gmail.com>
2024-04-25 13:57:11 +0530

    added env values

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 app.use(express.json());
 
